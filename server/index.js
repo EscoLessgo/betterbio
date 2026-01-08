76cc72c7a90d5945d889992db015cf408f9d64bb
TaverNxyz <voee178@yahoo.com>
2026-01-08 11:44:12 -0600

    chore: enable ADMIN_PASSWORD env var

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const ADMIN_PASSWORD = "Poncholove20!!";
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "Poncholove20!!";
 
 app.use(cors());
 app.use(express.json());
